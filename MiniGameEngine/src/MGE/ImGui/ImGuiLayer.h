10c5f8245c551ce86947880f745cbe7d175ae718
ZJU-Lv <77313676+ZJU-Lv@users.noreply.github.com>
2025-04-09 22:28:20 +0800

    添加ImGui事件（16p）

--- MiniGameEngine/src/MGE/ImGui/ImGuiLayer.h
+++ MiniGameEngine/src/MGE/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "MGE/Layer.h"
 
+#include "MGE/Events/ApplicationEvent.h"
+#include "MGE/Events/KeyEvent.h"
+#include "MGE/Events/MouseEvent.h"
+
 namespace MGE {
 
 	class MGE_API ImGuiLayer : public Layer
@@ -14,6 +18,16 @@ namespace MGE {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
