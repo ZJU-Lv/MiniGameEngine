d6c3d406fab066db4614848e9a7ee16e23f426a0
ZJU-Lv <77313676+ZJU-Lv@users.noreply.github.com>
2025-04-29 13:14:04 +0800

    优化绘制矩形的接口（58p）

--- MiniGameEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ MiniGameEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -194,6 +194,13 @@ namespace MGE {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetFloat(const std::string& name, float value)
+	{
+		MGE_PROFILE_FUNCTION();
+
+		UploadUniformFloat(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		MGE_PROFILE_FUNCTION();
