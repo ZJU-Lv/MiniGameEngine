f090a9d0812e891a1045629bce71d906ecb17a15
ZJU-Lv <77313676+ZJU-Lv@users.noreply.github.com>
2025-05-16 16:32:00 +0800

    FrameBuffer支持多个渲染目标（95p）

--- MGE-Editor/src/EditorLayer.cpp
+++ MGE-Editor/src/EditorLayer.cpp
@@ -24,6 +24,7 @@ namespace MGE {
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
 
 		FramebufferSpecification fbSpec;
+		fbSpec.AttachmentSpecification = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_Framebuffer = Framebuffer::Create(fbSpec);
